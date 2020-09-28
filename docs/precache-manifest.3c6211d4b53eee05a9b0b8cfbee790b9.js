bdb3b348b0bb82bb4e9ef0679e500299b644bb70
Jyotikant <sanjay.row.com@gmail.com>
2020-09-28 13:28:09 +0530

    build files update

---
+++ docs/precache-manifest.3c6211d4b53eee05a9b0b8cfbee790b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bac63a79025ad4b4f956802ba827e9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28023bd503e6d79f7fd7",
+    "url": "/static/css/2.cd846464.chunk.css"
+  },
+  {
+    "revision": "1dbfa92264f9989e4f83",
+    "url": "/static/css/main.08d2b17f.chunk.css"
+  },
+  {
+    "revision": "28023bd503e6d79f7fd7",
+    "url": "/static/js/2.58a5b14b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.58a5b14b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dbfa92264f9989e4f83",
+    "url": "/static/js/main.0270d887.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);
